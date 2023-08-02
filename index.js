e4dc18cdaa255e9bd9166a41ecd4f322b4f01ee2
devbathani <bathanid888@gmail.com>
2023-08-02 15:01:23 +0530

    server updates

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-import { WebSocketServer } from "ws";
+const WebSocket = require("ws");
 
-const wss = new WebSocketServer({ port: 8080 });
+// const serverAddress = "ws://127.0.0.1:5000";
+const serverAddress = "wss://otpless-websocket-tester.glitch.me/";
+
+const wss = new WebSocket(serverAddress, {
+  headers: {
+    "user-agent": "Mozilla",
+  },
+});
 
 wss.on("connection", function connection(ws) {
   ws.on("message", function message(data) {
     console.log("received: %s", data);
   });
   console.log("connected");
-  ws.send("Testing");
+  ws.send("devs testing");
 });
